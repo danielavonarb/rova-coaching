0f046d10ff99172c245bbb5464c396146e6748c3
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-08-26 19:35:44 +1200

    component setup

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header(){
   return (
-    <p>header</p>
+  <header>
+    <div className = "logo">
+      <img src = "./images/Logo_RGB.svg" id = "logo" alt=""></img>
+    </div>
+    <nav className = 'nav'>
+      <ul className = "nav__list">
+        <li className = "nav__item"><a href = "/mentaltraining" className = "nav__link">Mentaltraining</a></li>
+        <li className = "nav__item"><a href = "/about" className = "nav__link">Über mich</a></li>
+        <li className = "nav__item"><a href = "/kontakt" className = "nav__link">Kontakt</a></li>
+        <li className = "nav__item"><a href = "/infos" className = "nav__link">Weitere Infos</a></li>
+      </ul>
+    </nav>
+  </header>
   )
 }
 
