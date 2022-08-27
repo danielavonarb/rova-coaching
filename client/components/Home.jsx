ee9f5c7a41c39fcec4d44b13cef7930db63da42d
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-08-28 10:23:46 +1200

    Home component styled

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,7 +9,7 @@ function Home(){
         <h1 className="hero-title">Mutig und gelassen durchs Leben - ich helfe dir dabei</h1>
       </div>
       <div className="hero-img">
-		    <img className="image" src = "./images/home-pano02.jpg"></img>
+		    <img className="image" src = "./images/wasser01.jpeg"></img>
 	    </div>
       <Nav />
     </body>
