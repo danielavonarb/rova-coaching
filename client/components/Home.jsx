629e23185f36bfb13d1f73e1f109d53446a462e9
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-08-29 13:53:18 +1200

    Working on responsiveness

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Nav from './Nav'
 import Header from './Header'
 
 function Home(){
+
   return (
     <section id="body-home">
       <Header />
@@ -11,7 +12,11 @@ function Home(){
         <h1 className="hero-title">Mutig und gelassen durchs Leben - ich helfe dir dabei</h1>
       </div>
       <div className="hero-img">
-		    <img className="image" src = "./images/home-pano02.jpg"></img>
+        <div id="image"></div>
+        {/* <picture className="image">
+          <source media="(min-width: 900px)" srcSet="./images/home-pano02.jpg"></source>
+          <source media="(min-width: 375px)" srcSet="./images/IMG_4906.jpg"></source>
+        </picture> */}
 	    </div>
       <Nav />
     </section>
@@ -19,3 +24,4 @@ function Home(){
 }
 
 export default Home
+
