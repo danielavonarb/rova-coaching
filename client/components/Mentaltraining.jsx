56fc48c66527400c00eef9e9326e444a1af2148e
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-09-15 16:59:29 +1200

    embed youtube

--- client/components/Mentaltraining.jsx
+++ client/components/Mentaltraining.jsx
@@ -31,6 +31,9 @@ function Mentaltraining(){
       <div className="subtitle-container">
         <h3 className="subtitle">Dann hilft dir mein Coaching.</h3>
       </div>
+      <div className="youtube">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/3zYFWDJ26vM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
       <div className="beratung-content">
         <div className="beratung-container">
         <h4 className="beratung-title">Ablauf der Beratung</h4>
