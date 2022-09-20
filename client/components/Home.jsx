7fcfabf92ac948566ca8e687149c244c5246a2a5
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-09-21 11:35:09 +1200

    add hover effect nav

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -14,10 +14,6 @@ function Home(){
       </div>
       <div className="hero-img">
         <div id="image-home"></div>
-        {/* <picture className="image">
-          <source media="(min-width: 900px)" srcSet="./images/home-pano02.jpg"></source>
-          <source media="(min-width: 375px)" srcSet="./images/IMG_4906.jpg"></source>
-        </picture> */}
 	    </div>
       <Nav />
       <Footer />
